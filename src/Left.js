b62b80f70a6c81782db931b273ab0bdd7804a935
張文源 <c29429768@gmail.com>
2024-04-05 17:10:33 +0800

    Add navbar to Left

--- src/Left.js
+++ src/Left.js
@@ -1,10 +1,37 @@
-import './Left.css';
+import { IconHome, IconLogin, IconLogout } from "@tabler/icons-react";
+import "./Left.css";
+
+const data = [{ link: "", label: "Home", icon: IconHome }];
 
 function Left() {
+  const links = data.map((item) => (
+    <a
+      className="link"
+      data-active={undefined}
+      href={item.link}
+      key={item.label}
+    >
+      <item.icon className="linkIcon" stroke={1.5} />
+      <span>{item.label}</span>
+    </a>
+  ));
+
   return (
-    <div className="Left">
-		Left
-	</div>
+    <div className="left">
+      <nav className="navbar">
+        <div className="navbarMain">{links}</div>
+        <div className="footer">
+          <a className="link" href="#">
+            <IconLogin className="linkIcon" stroke={1.5} />
+            <span>Login</span>
+          </a>
+          <a className="link" href="#">
+            <IconLogout className="linkIcon" stroke={1.5} />
+            <span>Logout</span>
+          </a>
+        </div>
+      </nav>
+    </div>
   );
 }
 
