6b5531d970be44699c4b4d0d8dee481e34b6ea18
張文源 <c29429768@gmail.com>
2024-04-05 17:00:06 +0800

    Set up @mantine

--- src/index.js
+++ src/index.js
@@ -1,13 +1,21 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { createTheme, MantineProvider } from "@mantine/core";
+import "@mantine/core/styles.css";
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import "./index.css";
+import reportWebVitals from "./reportWebVitals";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const theme = createTheme({
+  /** Put your mantine theme override here */
+});
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <MantineProvider theme={theme}>
+      <App />
+    </MantineProvider>
   </React.StrictMode>
 );
 
