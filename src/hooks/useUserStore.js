aa08b28a669bcc5e224756abc2958f4d4027992f
張文源 <c29429768@gmail.com>
2024-04-06 17:10:05 +0800

    Fix: parsing sessionStorage

--- src/hooks/useUserStore.js
+++ src/hooks/useUserStore.js
@@ -1,15 +1,19 @@
 import { create } from "zustand";
 
-const useUserStore = create((set) => ({
-  user: sessionStorage.getItem("user") || null,
-  setUser: (user) => {
-    sessionStorage.setItem("user", user);
-    set({ user });
-  },
-  resetUser: () => {
-    sessionStorage.removeItem("user");
-    set({ user: null });
-  },
-}));
+const useUserStore = create((set) => {
+  const user = sessionStorage.getItem("user") || null;
+
+  return {
+    user: user ? JSON.parse(user) : null,
+    setUser: (user) => {
+      sessionStorage.setItem("user", JSON.stringify(user));
+      set({ user });
+    },
+    resetUser: () => {
+      sessionStorage.removeItem("user");
+      set({ user: null });
+    },
+  };
+});
 
 export default useUserStore;
